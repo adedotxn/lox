89e6cf15627b6d2af1f9e69ac694cccc4370cbaa
Philip <adedotxn.dev@gmail.com>
2025-02-09 02:01:04 +0000

    checkpoint: wrote visitor pattern for the AST

--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -47,6 +47,8 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         // defining the base class inside each subclass
         for(String type: types) {
             String className = type.split(":")[0].trim();
@@ -55,10 +57,34 @@ public class GenerateAst {
             defineType(writer, baseName, className, fields);
         }
 
+        // the base accept() method
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
 
         writer.println("}");
         writer.close();
     }
+
+    /**
+     * 
+     * @param writer
+     * @param baseName
+     * @param types
+     */
+    public static void defineVisitor(
+        PrintWriter writer, String baseName, List<String> types) {
+            writer.println("    interface Visitor<R> {");
+
+            for(String type: types) {
+                String typeName = type.split(":")[0].trim();
+                writer.println("      R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+            }
+
+        writer.println("    }");
+        writer.println();
+    }
+    
 
     /**
      * It declares each field in the class body. 
@@ -72,28 +98,35 @@ public class GenerateAst {
         PrintWriter writer, String baseName,
         String className, String fieldList
     ) {
-        writer.println(" static class " + className + " extends " + baseName + " {");
+        writer.println("    static class " + className + " extends " + baseName + " {");
 
       
         // Constructor
-        writer.println("    " + className + "(" + fieldList + ") {");
+        writer.println("        " + className + "(" + fieldList + ") {");
 
         // Store parameters in fields.
         String[] fields = fieldList.split(", ");
         for (String field : fields) {
         String name = field.split(" ")[1];
-        writer.println("      this." + name + " = " + name + ";");
+        writer.println("          this." + name + " = " + name + ";");
         }
 
-        writer.println("    }");
+        writer.println("        }");
+
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("          return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
 
         // Fields.
         writer.println();
         for (String field : fields) {
-        writer.println("    final " + field + ";");
+        writer.println("        final " + field + ";");
         }
 
-        writer.println("  }");
+        writer.println("    }");
+        writer.println();
     }
     
 }
