1574c84f540093abbc3abde1a2d9b7321b597c29
adedotxn <adedotxn.dev@gmail.com>
2025-02-21 23:29:59 +0100

    ch5, challenge3: visitor class for taking an expression, converting to RPN and printing resulting string

--- com/craftinginterpreters/lox/AstPrinter.java
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -42,15 +42,26 @@ class AstPrinter implements Expr.Visitor<String> {
         return builder.toString();
     }
 
-    // public static void main(String[] args) {
-    //     Expr expression = new Expr.Binary(
-    //         new Expr.Unary(
-    //             new Token(TokenType.MINUS, "-", null, 1),
-    //             new Expr.Literal(123)),
-    //         new Token(TokenType.STAR, "*", null, 1),
-    //         new Expr.Grouping(
-    //             new Expr.Literal(45.67)));
+    /**
+     * Example
+     * -123 * (45.67)
+     * becomes
+     * (* (- 123) (group 45.67))
+     * juts a string representation of our AST is what is produced. 
+     * the point is for visualisation
+     */
     
-    //     System.out.println(new AstPrinter().print(expression));
-    //   }
+    /** 
+    public static void main(String[] args) {
+       Expr expression = new Expr.Binary(
+           new Expr.Unary(
+               new Token(TokenType.MINUS, "-", null, 1),
+               new Expr.Literal(123)),
+           new Token(TokenType.STAR, "*", null, 1),
+           new Expr.Grouping(
+               new Expr.Literal(45.67)));
+    
+       System.out.println(new AstPrinter().print(expression));
+     } 
+    */
 }
